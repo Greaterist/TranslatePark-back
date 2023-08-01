5f53f01271a9dd8677557fde6729ec215542e565
Ivan Taktaev <32547753+Greaterist@users.noreply.github.com>
2023-08-01 13:10:22 +0000

    routes update

--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,13 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/test', [WordController::class, 'test']);
+
+Route::apiResource('/words', WordController::class);
+Route::apiResource('/languages', LanguageController::class);
+Route::apiResource('/translations', TranslationController::class);
+Route::apiResource('/translation_statuses', TranslationStatusController::class);
+Route::apiResource('/translation_votes', TranslationVoteController::class);
+Route::apiResource('/user_roles', UserRoleController::class);
+Route::apiResource('/word_comment_votes', WordCommentVoteController::class);
+Route::apiResource('/word_comments', Word_commentSeeder::class);
+Route::apiResource('/word_statuses', WordStatusController::class);
